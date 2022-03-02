97c4e170a26d0db397e764fc29987b7b382be9bf
Matt Foster <fmatthew40@gmail.com>
2022-03-01 18:55:24 -0700

    add route files

--- models/Thought.js
+++ models/Thought.js
@@ -4,8 +4,33 @@ const { Schema, model } = require('mongoose');
 
 
 const ReactionSchema = new Schema({
-
-})
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        // , ?  
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        Required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        // getter method to format timestamp
+    }
+    },
+{
+    toJSON: {
+        getters: true,
+    }
+    
+    }
+    )
 
 const ThoughtSchema = new Schema({
     thoughtText: {
